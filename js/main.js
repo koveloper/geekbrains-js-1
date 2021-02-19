315d814bbb60ecb2684d32c0d5f0afbee49c7f92
koveloper <koveloper@gmail.com>
2021-02-19 12:47:37 +0300

    homework-6

--- js/main.js
+++ js/main.js
@@ -1,235 +1,10 @@
-function lesson_1() {
-    var temperatureDeg = parseFloat(prompt("Введите температуру в градусах по Цельсию", 25));
-    var temperatureFar = (9 * temperatureDeg) / 5 + 32;
-    alert(`Температура в градусах по Цельсию: ${temperatureDeg}\r\nТемпература в градусах по Фаренгейту: ${temperatureFar}`);
-    var name_ = "Василий";
-    var admin_;
-    alert(`name: ${name_}\r\nadmin: ${admin_}`);
-    admin_ = name_;
-    alert(`name: ${name_}\r\nadmin: ${admin_}`);
-    alert(`JS-выражение 1000 + "108" будет равно ${1000 + "108"} т.к. движок JS выполнит конкатенацию строк, преобразовав первый аргумент в строку`);
-}
-
-function lesson_2() {
-    let a, b;
-
-    function sum(x, y) { return x + y; }
-
-    function multiply(x, y) { return x * y; }
-
-    function diff(x, y) { return x - y; }
-
-    function div(x, y) { return x / y; }
-
-    function makeOperation(x, y, opcode = "sum") {
-        switch (opcode) {
-            case "sum":
-                return sum(x, y);
-            case "multiply":
-                return multiply(x, y);
-            case "diff":
-                return diff(x, y);
-            case "div":
-                return div(x, y);
-            default:
-                return sum(x, y);
-        }
+function task() {
+    let products = document.createElement("div");
+    products.setAttribute("style", "display: flex; align-items: center; justify-content: center");
+    for(let p of market.products) {
+        products.appendChild(p.getDomElement());
     }
-    // let b = parseInt();
-    while (Number.isNaN(a = parseInt(prompt("Введите a")))) {}
-    while (Number.isNaN(b = parseInt(prompt("Введите b")))) {}
-    //
-    alert((a >= 0 && b >= 0) ? ("a - b = " + diff(a, b)) : ((a < 0 && b < 0) ? ("a * b = " + multiply(a, b)) : ("a + b = " + sum(a, b))));
-    //
-    let a_ = a;
-    a = a < 0 ? 0 : (a > 15 ? 15 : a);
-    if (a != a_) {
-        alert("Change a value. New value: " + a);
-    }
-    let froma = "[a=" + a + " to 15]: ";
-    switch (a) {
-        case 0:
-            froma += "0,";
-        case 1:
-            froma += "1,";
-        case 2:
-            froma += "2,";
-        case 3:
-            froma += "3,";
-        case 4:
-            froma += "4,";
-        case 5:
-            froma += "5,";
-        case 6:
-            froma += "6,";
-        case 7:
-            froma += "7,";
-        case 8:
-            froma += "8,";
-        case 9:
-            froma += "9,";
-        case 10:
-            froma += "10,";
-        case 11:
-            froma += "11,";
-        case 12:
-            froma += "12,";
-        case 13:
-            froma += "13,";
-        case 14:
-            froma += "14,";
-        case 15:
-            froma += "15";
-    }
-    alert(froma);
-    //------------------------------------
-    function _pow(val, pow) {
-        let nego = pow < 0;
-        pow = Math.abs(pow);
-        let p = pow == 0 ? 1 : (val * _pow(val, --pow));
-        return (nego ? (1 / p) : p);
-    }
-    let powResults = "";
-    let numbers = [2, 4, 10];
-    let pows = [0, 2, 3, 16, -1, -2];
-    for(let n of numbers) {
-        for(let p of pows) {
-            powResults += n + " ^ " + p + " = " + _pow(n, p) + "\r\n";
-        }    
-        powResults += "---------------------------\r\n";
-    }
-    console.log(powResults);
-}
-
-function lesson_3() {
-    let task_1 = ({start = 0, end = 100}) => {
-        function isPrimitive_(num) {
-            if(num <= 1) {
-                return false;
-            }
-            for(let i = 2; i < num; i++) {
-                if((num % i) == 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        let numbers = "";
-        for(let i = start; i <= end; i++) {
-            if(isPrimitive_(i)) {
-                numbers += numbers.length ? ("," + i) : ("" + i);
-            }
-        }
-        return numbers;
-    };
-
-    let task_2_3 = () => {
-        function countBasketPrice(arr) {
-            return arr.reduce((acc, el) => {return acc + el;}, 0);
-        }
-        let basket = [271, 10, 15, 78, 37, 1520, 1999];
-        return countBasketPrice(basket);
-    };
-
-    let task_4 = () => {
-        let result = [];
-        for(let i = 0; i < 10; result.push(i), i++){}
-        return result;
-    };
-
-    let task_5 = () => {
-        let result = "";
-        for(let i = 1; i <= 20; i++) {
-            for(let j = 0; j < i; j++) {
-                result += "X";                
-            }
-            result += "\r\n";
-        }        
-        return result;
-    };
-
-    return {
-        task_1,
-        task_2_3,
-        task_4,
-        task_5
-    }
-}
-
-function lesson_4() {
-    let task_1 = (num) => {
-        if(num === undefined) {
-            while(!num) {
-                num = Math.floor(Math.random() * Math.pow(10, Math.floor(Math.random() * 10)));
-            }        
-        }
-        let name = "";
-        let obj_ = {
-            _num: num,
-            toString: baseToStringFunc
-        };
-        while(num) {
-            name = name.length ? (name + "0") : "1";
-            obj_["_" + name] = num % 10;
-            num = Math.floor(num / 10); 
-        }
-        console.log(obj_);
-        return obj_;
-    }
-    return {
-        task_1
-    }
-}
-
-function lesson_5() {
-    let task_1 = () => {
-        let chessField = document.createElement("div");
-        let commonStyles = "display: flex; justify-content: center; align-items: center;"
-        let itemSizes = "width: 32px; height: 32px;"
-        chessField.setAttribute("style", commonStyles +  "flex-direction: column;");
-        for(let row = 0; row < 10; row++) {
-            let chessRow = document.createElement("div");
-            chessRow.setAttribute("style", commonStyles);
-            for(let col = 0; col < 10; col++) {
-                let chessItem = document.createElement("div");
-                if(row == 0 || row == 9) {
-                    chessItem.setAttribute("style", commonStyles + itemSizes);
-                    if(col != 0 && col != 9) {
-                        chessItem.textContent = String.fromCharCode(0x41 + (col - 1));
-                    }                    
-                } else {
-                    chessItem.setAttribute("style", 
-                            commonStyles 
-                            + itemSizes 
-                            + ((col != 0 && col != 9) ? "border: 1px solid black; box-sizing: border-box;" : "")
-                            + ((col != 0 && col != 9) ? ("background: " + (((row + col) % 2) ? "black;" : "bisque;")) : "")
-                    );
-                    if(col == 0 || col == 9) {
-                        chessItem.textContent = row;
-                    }
-                }
-                chessRow.appendChild(chessItem);
-            }
-            chessField.append(chessRow);
-        }
-        return chessField.outerHTML;
-    }
-    let task_2 = () => {
-        let buyClickCallback = (product) => {
-            console.log(product);
-        };
-        
-        let products = document.createElement("div");
-        products.setAttribute("style", "display: flex; align-items: center; justify-content: center");
-        for(let p of market.products) {
-            products.appendChild(p.getDomElement());
-        }
-        return products.outerHTML + market.cart.getDomElement().outerHTML;
-    }
-    return {
-        task_1,
-        task_2
-    }
+    return products.outerHTML + market.cart.getDomElement().outerHTML;
 }
 
 /**
@@ -237,20 +12,21 @@ function lesson_5() {
  */
 function Magazine() {
 
-    function Product(id, title, price, imageURL) {
+    function Product(id, title, price) {
         this.id = id;
         this.title = title;
         this.price = price;
-        this.imageURL = imageURL;
-        
+        this.imageURLs = [];
+        let currentImage_ = 0;        
         let defStyles = "display: flex; align-items: center; justify-content: center;";
         let domElement = document.createElement("div");
-        domElement.setAttribute("style", "min-width: 80px; border: 1px solid gray; border-radius: 4px; display: flex; flex-direction: column; align-items: center; justify-content: center; margin: 5px;");
+        domElement.classList.add("product");
         let header = document.createElement("div");
         header.setAttribute("style", defStyles + "padding: 0 8px; color: black");
         header.textContent = title;
         let image = document.createElement("div");
-        image.setAttribute("style", defStyles + "padding: 0 8px");
+        image.classList.add("product-image");
+        image.setAttribute("onclick", `market.getProduct(${id}).openInModal()`);
         let footer = document.createElement("div");
         footer.setAttribute("style", defStyles + "padding: 0 8px; color: lightgray");
         footer.textContent = price + " р.";
@@ -262,12 +38,70 @@ function Magazine() {
         domElement.appendChild(image);
         domElement.appendChild(footer);
         domElement.appendChild(buy);
-        buy.onclick = function() {
-            market.cart.add(id);
-        }
-        // buy.setAttribute("onclick", `market.cart.add(${id})`);
+        buy.setAttribute("onclick", `market.cart.add(${id})`);
+
         this.getDomElement = function() {
             return domElement;
+        }
+        this.addImage = function(mainImgURL, smallImgURL) {
+            if(mainImgURL && smallImgURL) {
+                this.imageURLs.push([mainImgURL, smallImgURL]);
+            } else if(mainImgURL) {
+                this.imageURLs.push([mainImgURL, mainImgURL]);
+            } else if(smallImgURL) {
+                this.imageURLs.push([smallImgURL, smallImgURL]);
+            }
+            if(this.imageURLs.length == 1) {
+                let img = document.createElement("img");
+                img.setAttribute("src", this.imageURLs[0][1]);
+                img.setAttribute("alt", "product image");
+                img.setAttribute("width", "128");
+                img.setAttribute("style", "width: 100%; border-radius: 8px");
+                image.append(img);
+            }
+            return this;
+        }
+        this.getId = function() {
+            return this.id;
+        }
+        this.openInModal = function() {
+            console.log("openInModal " + id);
+            if(!document.querySelector("#product-modal")) {
+                let modalWrapper = document.createElement("div");
+                modalWrapper.classList.add("product-modal-wrapper");
+                modalWrapper.setAttribute("id", "product-modal");
+                modalWrapper.setAttribute("onclick", `document.querySelector('#product-modal').style.maxHeight = 0`)
+                let modal = document.createElement("div");
+                modal.classList.add("product-modal");                
+                modalWrapper.appendChild(modal);
+                document.body.appendChild(modalWrapper);
+            }
+            let imgDiv = document.querySelector("#product-modal").querySelector(".product-modal");
+            imgDiv.textContent = '';
+            currentImage_ = 0;
+            for(let i = 0; i < this.imageURLs.length; i++) {
+                let imgRow = this.imageURLs[i];
+                let img = document.createElement("img");
+                img.setAttribute("src", imgRow[0]);
+                img.setAttribute("alt", "product image");
+                img.setAttribute("width", "128");
+                img.setAttribute("onclick", `market.getProduct(${id}).toggleImage(event)`);
+                img.setAttribute("style", `width: ${i == currentImage_ ? "70%" : "0"}; border-radius: 8px; transition: 0.3s`);
+                imgDiv.append(img);
+            }
+            
+            setTimeout(() => {
+                document.querySelector("#product-modal").style.maxHeight = '100%';
+            }, 20);
+        }
+        this.toggleImage = function(e) {
+            e.stopPropagation();
+            currentImage_ = (currentImage_ + 1) % this.imageURLs.length;
+            let imgDiv = document.querySelector("#product-modal").querySelector(".product-modal");
+            let images = document.querySelector("#product-modal").querySelector(".product-modal").querySelectorAll("img");
+            for(let i = 0; i < images.length; i++) {
+                images[i].style.width = (i == currentImage_) ? "70%" : "0";
+            }
         }
     }
 
@@ -334,98 +168,38 @@ function Magazine() {
         }
     }
 
+    let id = 1;
     let products = [
-        new Product("1", "Хлеб", "55", ""),
-        new Product("2", "Масло", "160", ""),
-        new Product("3", "Молоко", "75", ""),
-        new Product("4", "Яблочный сок", "120", ""),
-        new Product("5", "Печенье овсяное", "68", ""),
+        new Product(id++, "Хлеб", "55", "")
+            .addImage("img/bread-0.png", "img/bread-0-min.png")
+            .addImage("img/bread-1.png", "img/bread-1-min.png")
+            .addImage("img/bread-2.png", "img/bread-2-min.png"),
+        new Product(id++, "Масло", "160", "")
+            .addImage("img/butter-0.jpg", "img/butter-0-min.png"),
+        new Product(id++, "Молоко", "75", "")
+            .addImage("img/milk-0.jpg", "img/milk-0-min.png")        
+            .addImage("img/milk-1.jpg", "img/milk-1-min.png"),        
+        new Product(id++, "Яблочный сок", "120", "")
+            .addImage("img/apple-juice-0.jpg", "img/apple-juice-0-min.png")
+            .addImage("img/apple-juice-1.jpg", "img/apple-juice-1-min.png"),        
+        new Product(id++, "Печенье овсяное", "68", "")
+            .addImage("img/snacks-0.jpg", "img/snacks-0-min.png")
+            .addImage("img/snacks-1.jpg", "img/snacks-1-min.png"),
     ];
 
     let cart = new Cart();
 
     return {
         products,
+        getProduct: function(id) {
+            for(let p of products) {
+                if(p.getId() == id) {
+                    return p;
+                }
+            }
+        },
         cart
     }
 }
 
 let market = new Magazine();
-
-
-/**
- * COMMON USED FUNCTIONS
- */
-function baseToStringFunc() {
-    let str_ = "";
-    for(let k in this) {
-        if(k != "_num" && typeof(this[k]) != "function") {
-            str_ += (str_.length ? ", " : "") + k + ": " + this[k];
-        }
-    }
-    return this._num + " = {" + str_ + "}"
-}
-
-function pretifyJsFunc(func, args, outResult = true) {
-    let code = '' + func;
-    let html = "<div style='display: flex; flex-direction: column;'>";
-    html += "<span style='margin: 8px 0; border: 1px solid gray'></span>";
-    let marginStep = 16;
-    let margin = 0;
-    function wrap(line, margin) {
-        line = line.replaceAll("function", "<span style='color: blue'>function</span>");
-        line = line.replaceAll("let ", "<span style='color: blue'>let </span>");
-        line = line.replaceAll("new ", "<span style='color: blue'>new </span>");
-        line = line.replaceAll("for", "<span style='color: magenta'>for</span>");
-        line = line.replaceAll("if", "<span style='color: magenta'>if</span>");
-        line = line.replaceAll("else", "<span style='color: magenta'>else</span>");
-        line = line.replaceAll("while", "<span style='color: magenta'>while</span>");
-        line = line.replaceAll("return", "<span style='color: brown'>return</span>");
-        line = line.replaceAll("true", "<span style='color: cornflowerblue'>true</span>");
-        line = line.replaceAll("false", "<span style='color: cornflowerblue'>false</span>");
-        return `<span style='margin: 0 0 0 ${margin}px'>${line}</span>`;
-    }
-
-    function calc(line, symbol) {
-        let n = 0;
-        for(let s of line) {
-            n+= s == symbol;
-        }
-        return n;
-    }
-
-    for(let line of code.split("\r\n")) {
-        if(line.includes("}")) {
-            margin -= marginStep * calc(line, '}');
-            if(calc(line, '{') == calc(line, '}')) {
-                margin += marginStep * calc(line, '{');                
-            }
-        }
-        if(line.includes("]")) {
-            margin -= marginStep * calc(line, ']');
-            if(calc(line, '[') == calc(line, ']')) {
-                margin += marginStep * calc(line, '[');                
-            }
-        }
-        html += wrap(line, margin < 0 ? 0 : margin);
-        if(line.includes("{")) {
-            margin += marginStep * calc(line, '{');
-            if(calc(line, '{') == calc(line, '}')) {
-                margin -= marginStep * calc(line, '}');                
-            }
-        }
-        if(line.includes("[")) {
-            margin += marginStep * calc(line, '[');
-            if(calc(line, '[') == calc(line, ']')) {
-                margin -= marginStep * calc(line, ']');                
-            }
-        }
-    }
-    if(outResult) {
-        html += "<span style='margin: 8px 0; border: 1px solid gray'></span>";
-        if(typeof func == "function") {
-            html += `<div style="font-weight: 600; color: green">${func(args)}</div>`;
-        }        
-    }
-    return html + "</div>";
-}
